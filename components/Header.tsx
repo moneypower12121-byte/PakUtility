2094a032778a074650bca24c11fc2d9568508410
Noman Afridi <nomanafridi941@gmail.com>
2025-12-31 19:11:01 +0500

    Replace SVG logo with Logo.png image in header

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,14 +7,7 @@ const Header: React.FC = () => {
     <header className="bg-emerald-700 text-white sticky top-0 z-50 shadow-md">
       <div className="container mx-auto px-4 py-3 flex flex-wrap justify-between items-center">
         <Link to="/" className="flex items-center space-x-2" aria-label="Pak Utility Tools Home">
-          <span className="inline-block">
-            {/* Modern SVG Logo */}
-            <svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <rect x="2" y="2" width="32" height="32" rx="8" fill="#059669"/>
-              <path d="M11 25V11h4.5c2.5 0 4 1.2 4 3.2 0 1.2-.7 2.2-1.8 2.7v.1c1.6.4 2.8 1.5 2.8 3.2C20.5 23 18.7 25 15.7 25H11zm3.7-8.2c1.3 0 2-.6 2-1.7 0-1.1-.7-1.7-2-1.7H13.5v3.4h1.2zm.3 6.1c1.5 0 2.3-.7 2.3-2 0-1.3-.8-2-2.3-2h-1.5v4h1.5z" fill="#fff"/>
-              <circle cx="27" cy="9" r="3" fill="#34D399" stroke="#fff" strokeWidth="1.5"/>
-            </svg>
-          </span>
+          <img src={require('../Logo.png')} alt="Pak Utility Tools Logo" className="w-10 h-10 rounded shadow bg-white object-contain" />
           <span className="text-2xl font-bold tracking-tight ml-2">PakUtility<span className="text-emerald-200">Tools</span></span>
         </Link>
         <nav className="hidden md:flex space-x-6 font-medium">
